0393f3a6b1ca6e8f3acaf21dafd4cb173fc391ce
root <mgiraudo@ies21.edu.ar>
2024-04-10 13:21:26 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryJueguitoPiola
+{
+    public partial class frmJueguito : Form
+    {
+        public frmJueguito()
+        {
+            InitializeComponent();
+        }
+    }
+}
